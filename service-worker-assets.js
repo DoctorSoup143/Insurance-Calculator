b5f624e8bb97b21e0ab302e5f3721ce8df8a1501
DoctorSoup143 <sdrobish4@gmail.com>
2023-05-13 17:03:02 -0400

    Page Fix

--- service-worker-assets.js
+++ service-worker-assets.js
@@ -33,11 +33,11 @@
       "url": "_framework\/icudt_no_CJK.dat"
     },
     {
-      "hash": "sha256-4d\/UnlqxzHdK3T0aXYiIe7SjIymEqnw4+Ny7srBUYRs=",
+      "hash": "sha256-Oh0Hd\/AJwHOBvn19I+K3xTi2G8rcWQ6Ulzvi70cG9Po=",
       "url": "_framework\/blazor.boot.json"
     },
     {
-      "hash": "sha256-HcwwV\/1a\/+zQmW6UFHfPEvfEvMU3t2apDSffWt4H08Y=",
+      "hash": "sha256-1EdohAmnUJgtN41UwhvdqTSlIi+Y0ZpO37AsyTy58qY=",
       "url": "_framework\/Health Care Calc.dll"
     },
     {
@@ -221,5 +221,5 @@
       "url": "manifest.json"
     }
   ],
-  "version": "dSC1RBbr"
+  "version": "\/yo\/4hi\/"
 };
--- service-worker.js
+++ service-worker.js
@@ -46,4 +46,4 @@ async function onFetch(event) {
 
     return cachedResponse || fetch(event.request);
 }
-/* Manifest version: dSC1RBbr */
+/* Manifest version: /yo/4hi/ */
